46728ae58bd3de12389d70defff61ccfa9f5040d
phanquockhanh123 <khanhphanquoc68@gmail.com>
2024-04-21 22:41:31 +0700

    init project

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package javacore;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java core!");
+		System.out.println("Hello world with java core!");
+		System.out.println("Hello world with java core!");
+	}
+}
